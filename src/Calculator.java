d6d48f75bdb0af20674ae92b886357d0ca38757d
mdesan <mdesantos2002@gmail.com>
2024-08-23 20:28:21 -0400

    still need to implement negation and exceptions

--- src/Calculator.java
+++ src/Calculator.java
@@ -88,7 +88,6 @@ public class Calculator {
             });
         }
 
-
         //function buttons----------------------------------------------------
 
         //addition button
--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -1,8 +1,6 @@
 public class CalculatorApp extends Calculator{
 
     public static void main(String[] args) {
-
         Calculator c = new Calculator();
-
     }
 }
